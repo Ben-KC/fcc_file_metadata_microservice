9b7801dcf803822219c32709d2e64a0d4bf1ae64
Benjamin Cox <benny24c@gmail.com>
2016-11-14 22:02:11 +0000

    adds handling for filesize post request

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ var upload = multer();
 var app = express();
 app.set('port', process.env.PORT ||  8080);
 
+//index/upload page
 app.use('/', express.static('public'));
+
+//handle post request
+app.post('/filesize', upload.single('analyse'), function(req, res) {
+    //if no file is uploaded
+    if(!req.file) {
+        return res.send(JSON.stringify({error: 'no file selected'}));
+    }
+
+    res.send(JSON.stringify({size: req.file.size}));
+});
 
 app.listen(app.get('port'));
